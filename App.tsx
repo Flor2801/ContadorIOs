2abde5bd7fad087185e547c374faf72ff125e486
Flor2801 <florenciasarria1@hotmail.com>
2023-05-16 15:00:34 -0300

    First Commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+    return (
+    <View>
+      <Text>Hola mundo</Text>
+    </View>
+    )
+  }
+
+export default App;
